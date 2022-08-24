0288321d9129b1ed210af781a41bae8e864dcc5b
Michael Weisgrab <michael.weisgrab@gmail.com>
2022-08-24 22:51:25 +0200

    circle ci (#6)
    add circle ci pipeline

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,23 @@
 const nxPreset = require('@nrwl/jest/preset').default;
 
-module.exports = { ...nxPreset };
+if (process.env.CI) {
+  nxPreset.reporters = [
+    [
+      'jest-junit',
+      {
+        outputDirectory: './reports/test',
+        outputName: 'test-report',
+        uniqueOutputName: 'true',
+        suiteName: 'Jest tests',
+        classNameTemplate: '{classname} / {title}',
+        titleTemplate: '{classname} / {title}',
+        ancestorSeparator: ' / ',
+        usePathForSuiteName: 'true',
+      }
+    ]
+  ]
+}
+
+module.exports = {
+  ...nxPreset
+};
