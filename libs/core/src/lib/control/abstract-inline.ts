605412cfd36a3d4ccca7a2027592a1525c8f0dee
Michael Weisgrab <michael.weisgrab@gmail.com>
2022-08-24 20:29:48 +0200

    fix linter warnings, add license

--- libs/core/src/lib/control/abstract-inline.ts
+++ libs/core/src/lib/control/abstract-inline.ts
@@ -1,16 +1,7 @@
-import { AbstractControl, FormGroup } from "@angular/forms";
-import { Question, QuestionProperties, ValidationOptions } from '../model';
+import { FormGroup } from '@angular/forms';
+import { Question } from '../model';
 
 export abstract class FastFormInline {
   formGroup!: FormGroup;
-  // control!: AbstractControl;
   questions!: Array<Question>;
-
-  // public get validation(): ValidationOptions | undefined {
-  //   return this.question.validation;
-  // }
-
-  // protected get baseProperties(): QuestionProperties {
-  //   return this.question?.properties || {};
-  // }
 }
