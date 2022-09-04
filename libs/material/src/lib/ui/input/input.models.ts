d315e91e57036adf6afdb2c09b7b80244ea9ee4e
Michael Weisgrab <michael.weisgrab@gmail.com>
2022-09-04 21:40:31 +0200

    fix question properties types, update version

--- libs/material/src/lib/ui/input/input.models.ts
+++ libs/material/src/lib/ui/input/input.models.ts
@@ -1,6 +1,8 @@
+import { QuestionProperties } from '@ngx-fast-forms/core';
+
 export type InputFormat = 'text' | 'number' | 'currency';
 
-export interface InputProperties {
+export interface InputProperties extends QuestionProperties {
   format?: InputFormat;
   attributes?: { [key: string]: string };
 }
