79b971ad0f00194421cee8fe79a94a2fbefd10e4
Michael Weisgrab <michael.weisgrab@gmail.com>
2022-09-23 23:00:43 +0200

    feat: add standard-version package (#26)

--- version.sh
+++ version.sh
@@ -2,14 +2,15 @@
 
 set -e
 
-versionFiles=( "." "libs/core" "libs/material" "libs/material-experimental" )
+versionFiles=( "libs/core" "libs/material" "libs/material-experimental" )
 
-#jq ".version = \"$1\"" package.json
+version=$(jq -r .version package.json)
+echo "Set version to $version"
 
 for i in "${versionFiles[@]}"; do
   packageFile="$i/package.json"
-  echo "$packageFile"
+  echo "Update '$packageFile'"
   cp "$packageFile" "$packageFile-old"
-  jq ".version = \"$1\"" "$packageFile-old" > "$packageFile"
+  jq ".version = \"$version\"" "$packageFile-old" > "$packageFile"
   rm "$packageFile-old"
 done
