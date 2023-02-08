21e03ef47df480d1548f4d72b424af34acef7241
Michael Weisgrab <michael.weisgrab@gmail.com>
2023-02-08 20:37:06 +0100

    temp: fix linter

--- libs/core/src/lib/internal/inject.util.ts
+++ libs/core/src/lib/internal/inject.util.ts
@@ -2,26 +2,28 @@ import { AbstractControl } from '@angular/forms';
 import { StaticProvider } from '@angular/core';
 import { CONTROL_PROPERTIES } from '../components/util/inject-token';
 import { QuestionDefinition } from '../components/question-definition';
-import { FastFormArray } from '../control/fast-form-array';
-import { FastFormControl } from '../control/fast-form-control';
-import { FastFormGroup } from '../control/fast-form-group';
 import { Question } from '../model';
 
 export function createDefaultProviders(control: AbstractControl, question?: Question): StaticProvider[] {
   const providers: StaticProvider[] = [];
-  if (control instanceof FastFormGroup ||
-      control instanceof FastFormControl ||
-      control instanceof FastFormArray) {
-    if (control.question) {
-      providers.push({
-        provide: QuestionDefinition,
-        useValue: new QuestionDefinition(question ? question : control.question)
-      });
-    }
+  if (isQuestionProvider(control)) {
+    providers.push({
+      provide: QuestionDefinition,
+      useValue: new QuestionDefinition(question ? question : control.question)
+    });
     providers.push({
       provide: CONTROL_PROPERTIES,
       useValue: question ? question.properties ?? {} : control.question?.properties ?? {}
     });
   }
   return providers;
+}
+
+interface QuestionProvider {
+  question: Question;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function isQuestionProvider(obj: any): obj is QuestionProvider {
+  return 'question' in obj;
 }
